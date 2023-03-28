3fe4c2ef493a6fefae94c4482d0ff5547098fc40
Andrii Petrinko <apetrinko@gmail.com>
2023-03-28 22:48:34 +0300

    routes are working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 
 export const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider,
 } from 'react-router-dom';
 import './App.scss';
 import './styles/_fontsTypography.scss';
 import { App } from './App';
+import { Features } from './components/FeaturesPage/Features';
+import { Home } from './components/HomePage/Home';
+import { Blog } from './components/BlogPage/Blog';
+import { Shop } from './components/ShopPage/Shop';
+import { About } from './components/AboutPage/About';
+import { Contact } from './components/ContactPage/Contact';
+// import { Blog } from './components/BlogPage/Blog';
+// import { Shop } from './components/ShopPage/Shop';
+// import { About } from './components/AboutPage/About';
+// import { Contact } from './components/ContactPage/Contact';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      {/* ... etc. */}
-    </Route>,
-  ),
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'features',
+        element: <Features />,
+      },
+      {
+        path: 'blog',
+        element: <Blog />,
+      },
+      {
+        path: 'shop',
+        element: <Shop />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
