bfcca84f4365c11445a07d7b331058c077c6b44c
Andrii Petrinko <apetrinko@gmail.com>
2023-03-29 00:44:59 +0300

    try to deploy without routes

--- src/components/Header/NavList.tsx
+++ src/components/Header/NavList.tsx
@@ -1,6 +1,114 @@
+// import React, { memo } from 'react';
+// import cn from 'classnames';
+// import { Link, useLocation } from 'react-router-dom';
+
+// type Props = {
+//   isOpenMenu: boolean;
+//   setIsOpen: (value: React.SetStateAction<boolean>) => void;
+// };
+
+// export const NavList: React.FC<Props> = memo((props: Props) => {
+//   const {
+//     isOpenMenu,
+//     setIsOpen,
+//   } = props;
+
+//   const location = useLocation();
+
+//   return (
+//     <nav className={(cn('navigation', { navigation__mobile: !isOpenMenu }))}>
+//       <ul className="navigation__list">
+//         <li className="navigation__item">
+//           <Link
+//             to="/"
+//             className={cn('navigation__link', {
+//               'navigation__link--active': location.pathname === '/',
+//             })}
+//           >
+//             Home
+//           </Link>
+//         </li>
+//         <li className="navigation__item">
+//           <Link
+//             to="/features"
+//             className={cn('navigation__link', {
+//               'navigation__link--active': location.pathname === '/features',
+//             })}
+//           >
+//             Features
+//           </Link>
+//         </li>
+//         <li className="navigation__item">
+//           <Link
+//             to="/blog"
+//             className={cn('navigation__link', {
+//               'navigation__link--active': location.pathname === '/blog',
+//             })}
+//           >
+//             Blog
+//           </Link>
+//         </li>
+//         <li className="navigation__item">
+//           <Link
+//             to="/shop"
+//             className={cn('navigation__link', {
+//               'navigation__link--active': location.pathname === '/shop',
+//             })}
+//           >
+//             Shop
+//           </Link>
+//         </li>
+//         <li className="navigation__item">
+//           <Link
+//             to="/about"
+//             className={cn('navigation__link', {
+//               'navigation__link--active': location.pathname === '/about',
+//             })}
+//           >
+//             About
+//           </Link>
+//         </li>
+//         <li className="navigation__item">
+//           <Link
+//             to="/contact"
+//             className={cn('navigation__link', {
+//               'navigation__link--active': location.pathname === '/contact',
+//             })}
+//           >
+//             Contact
+//           </Link>
+//         </li>
+//       </ul>
+
+//       <div className="navigation__user">
+//         <ul className="navigation__user__list">
+//           <button
+//             type="button"
+//             onClick={() => setIsOpen(false)}
+//             className="link__icon"
+//           >
+//             <li className="navigation__user__item cartNav">
+//               <img src="" alt="user" />
+//             </li>
+//           </button>
+
+//           <button
+//             type="button"
+//             onClick={() => setIsOpen(false)}
+//             className="link__icon"
+//           >
+//             <li className="navigation__user__item cartNav">
+//               <img src="" alt="user" />
+//             </li>
+//           </button>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// });
+
 import React, { memo } from 'react';
 import cn from 'classnames';
-import { Link, useLocation } from 'react-router-dom';
 
 type Props = {
   isOpenMenu: boolean;
@@ -13,70 +121,26 @@ export const NavList: React.FC<Props> = memo((props: Props) => {
     setIsOpen,
   } = props;
 
-  const location = useLocation();
-
   return (
     <nav className={(cn('navigation', { navigation__mobile: !isOpenMenu }))}>
       <ul className="navigation__list">
         <li className="navigation__item">
-          <Link
-            to="/"
-            className={cn('navigation__link', {
-              'navigation__link--active': location.pathname === '/',
-            })}
-          >
-            Home
-          </Link>
+          <a href="/" className="navigation__link">Home</a>
         </li>
         <li className="navigation__item">
-          <Link
-            to="/features"
-            className={cn('navigation__link', {
-              'navigation__link--active': location.pathname === '/features',
-            })}
-          >
-            Features
-          </Link>
+          <a href="#features" className="navigation__link">Features</a>
         </li>
         <li className="navigation__item">
-          <Link
-            to="/blog"
-            className={cn('navigation__link', {
-              'navigation__link--active': location.pathname === '/blog',
-            })}
-          >
-            Blog
-          </Link>
+          <a href="#blog" className="navigation__link">Blog</a>
         </li>
         <li className="navigation__item">
-          <Link
-            to="/shop"
-            className={cn('navigation__link', {
-              'navigation__link--active': location.pathname === '/shop',
-            })}
-          >
-            Shop
-          </Link>
+          <a href="#shop" className="navigation__link">Shop</a>
         </li>
         <li className="navigation__item">
-          <Link
-            to="/about"
-            className={cn('navigation__link', {
-              'navigation__link--active': location.pathname === '/about',
-            })}
-          >
-            About
-          </Link>
+          <a href="#about" className="navigation__link">About</a>
         </li>
         <li className="navigation__item">
-          <Link
-            to="/contact"
-            className={cn('navigation__link', {
-              'navigation__link--active': location.pathname === '/contact',
-            })}
-          >
-            Contact
-          </Link>
+          <a href="#contact" className="navigation__link">Contact</a>
         </li>
       </ul>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,60 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from 'react-router-dom';
+// import './App.scss';
+// import './styles/_fontsTypography.scss';
+// import { App } from './App';
+// import { Features } from './components/FeaturesPage/Features';
+// import { Home } from './components/HomePage/Home';
+// import { Blog } from './components/BlogPage/Blog';
+// import { Shop } from './components/ShopPage/Shop';
+// import { About } from './components/AboutPage/About';
+// import { Contact } from './components/ContactPage/Contact';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />,
+//       },
+//       {
+//         path: 'features',
+//         element: <Features />,
+//       },
+//       {
+//         path: 'blog',
+//         element: <Blog />,
+//       },
+//       {
+//         path: 'shop',
+//         element: <Shop />,
+//       },
+//       {
+//         path: 'about',
+//         element: <About />,
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
 import './App.scss';
 import './styles/_fontsTypography.scss';
 import { App } from './App';
-import { Features } from './components/FeaturesPage/Features';
-import { Home } from './components/HomePage/Home';
-import { Blog } from './components/BlogPage/Blog';
-import { Shop } from './components/ShopPage/Shop';
-import { About } from './components/AboutPage/About';
-import { Contact } from './components/ContactPage/Contact';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'features',
-        element: <Features />,
-      },
-      {
-        path: 'blog',
-        element: <Blog />,
-      },
-      {
-        path: 'shop',
-        element: <Shop />,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'contact',
-        element: <Contact />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
