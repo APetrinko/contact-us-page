39276558b5fd862830f0866efb442eb5e04799f5
Andrii Petrinko <apetrinko@gmail.com>
2023-03-29 10:35:21 +0300

    fixing routes for deploying

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import './styles/_fontsTypography.scss';
 import { App } from './App';
@@ -14,42 +11,20 @@ import { Shop } from './components/ShopPage/Shop';
 import { About } from './components/AboutPage/About';
 import { Contact } from './components/ContactPage/Contact';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: 'features',
-        element: <Features />,
-      },
-      {
-        path: 'blog',
-        element: <Blog />,
-      },
-      {
-        path: 'shop',
-        element: <Shop />,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'contact',
-        element: <Contact />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="" element={<Home />} />
+          <Route path="features" element={<Features />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
