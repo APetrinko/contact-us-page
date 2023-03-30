ba8d56c7e179ca00e25a8a4e9508d42838f423f8
Andrii Petrinko <apetrinko@gmail.com>
2023-03-30 18:05:15 +0300

    added icons for phone, mail and adnress

--- src/components/ContactPage/Contact.tsx
+++ src/components/ContactPage/Contact.tsx
@@ -3,6 +3,9 @@ import { ContactIpunts } from './ContactInputs';
 import insta from '../../styles/imgs/insta.svg';
 import twitter from '../../styles/imgs/twitter.svg';
 import discord from '../../styles/imgs/discord.svg';
+import phone from '../../styles/imgs/phone.svg';
+import mail from '../../styles/imgs/mail.svg';
+import location from '../../styles/imgs/location.svg';
 
 export const Contact: React.FC = memo(() => {
   console.log('render contact'); // eslint-disable-line no-console
@@ -23,15 +26,15 @@ export const Contact: React.FC = memo(() => {
 
           <div className="contact__contacts">
             <div className="contact__contacts__phone">
-              <img src="" alt="" />
+              <img src={phone} alt="phone" className="contact__contacts__phone-img" />
               <a href="tel: +10123456789" className="contact__contacts__phone-number">+1012 3456 789</a>
             </div>
             <div className="contact__contacts__mail">
-              <img src="" alt="" />
+              <img src={mail} alt="phone" className="contact__contacts__mail-img" />
               <a href="mailto: demo@gmail.com" className="contact__contacts__mail-link">demo@gmail.com</a>
             </div>
             <div className="contact__contacts__adress">
-              <img src="" alt="" />
+              <img src={location} alt="location" className="contact__contacts__adress-img" />
               <a href="/" className="contact__contacts__adress-street">132 Dartmouth Street Boston, Massachusetts 02156 United States</a>
             </div>
           </div>
@@ -39,17 +42,17 @@ export const Contact: React.FC = memo(() => {
           <div className="contact__social-media">
             <div className="contact__twitter">
               <a href="https://twitter.com/">
-                <img src={twitter} alt="" />
+                <img src={twitter} alt="twitter" />
               </a>
             </div>
             <div className="contact__instagram">
               <a href="https://instagram.com">
-                <img src={insta} alt="" />
+                <img src={insta} alt="instagram" />
               </a>
             </div>
             <div className="contact__discord">
               <a href="https://discord.com">
-                <img src={discord} alt="" />
+                <img src={discord} alt="discord" />
               </a>
             </div>
           </div>
